ce31f6d3893dc70a147242006b2cb59bb6306ed7
abdennourelamraoui354-glitch <abdennourelamraoui354@gmail.com>
2026-06-04 13:11:00 +0200

    Add files via upload

---
+++ fitgum-config[1].js
@@ -0,0 +1,7 @@
+(function () {
+  var DEFAULT_URL = "https://script.google.com/macros/s/AKfycbyYwJnMo1pyOauCBsSun3lO0dbkdLw6VS8eZYdnLkAmdkEDMSkwGKvtH5STpiWougHJ/exec";
+
+  window.FITGUM_CONFIG = {
+    SHEET_URL: DEFAULT_URL
+  };
+})();
